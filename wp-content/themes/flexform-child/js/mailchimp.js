bbf4db6a6aeab3dd7ca4c6446bacd801c6e0e0b8
Saki Sato <sakiisato@gmail.com>
2015-03-19 20:13:24 -0400

    commiting mailchimp files and removing copyrighted material

--- wp-content/themes/flexform-child/js/mailchimp.js
+++ wp-content/themes/flexform-child/js/mailchimp.js
@@ -66,7 +66,7 @@ function mce_init_form(){
       var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
       var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
       $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
-      options = { url: 'http://sakisato.us5.list-manage.com/subscribe/post-json?u=63744d3b2ea40a81b53a5d119&id=552f7e1d30&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+      options = { url: 'http:////canariesinthecoalmine.us9.list-manage.com/subscribe/post?u=76e7982baabc480dc91a48617&amp;id=eb11409c83', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
                     beforeSubmit: function(){
                         $('#mce_tmp_error_msg').remove();
                         $('.datefield','#mc_embed_signup').each(
